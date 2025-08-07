8a3f50960865b0f045a3df6f884238466930c4d0
willkl2415 <68011693+willkl2415@users.noreply.github.com>
2025-08-07 11:47:50 +0100

    create roles.js

---
+++ ATIS_External/roles.js
@@ -0,0 +1,96 @@
+
+const roleData = {
+  "Training and Learning & Development (L&D)": {
+    "Leadership & Strategy": [
+      "Chief Learning Officer (CLO)",
+      "Director of Training and Development",
+      "Vice President (VP) of Learning & Development",
+      "Head of Learning & Development",
+      "Learning and Development Manager",
+      "Training Manager",
+      "Director of Knowledge & Learning",
+      "Talent Development Manager",
+      "Training Program Manager",
+      "Leadership Development Specialist"
+    ],
+    "Program Design & Development": [
+      "Instructional Designer",
+      "Senior Instructional Designer",
+      "Instructional Design Manager",
+      "Curriculum Designer",
+      "Curriculum Manager",
+      "Curriculum Consultant",
+      "Content Developer",
+      "Content Curator",
+      "Digital Learning Designer",
+      "eLearning Developer",
+      "eLearning Manager",
+      "Learning Architect",
+      "Learning Strategist",
+      "Learning Experience Designer",
+      "Learning Technologist",
+      "Instructional Technologist"
+    ],
+    "Training Delivery & Facilitation": [
+      "Corporate Trainer",
+      "Senior Trainer",
+      "Training Facilitator",
+      "Technical Trainer",
+      "IT Trainer",
+      "Sales Trainer",
+      "Customer Service Trainer",
+      "Onboarding Trainer",
+      "Subject Matter Expert / Trainer",
+      "Apprenticeship Trainer",
+      "Training Instructor",
+      "Learning Coach",
+      "Employee Development Coach"
+    ],
+    "Coordination, Analysis & Support": [
+      "Learning and Development Coordinator",
+      "Training Coordinator",
+      "L&D Administrator",
+      "Training Assistant",
+      "Learning & Development Assistant",
+      "Learning Systems Coordinator",
+      "LMS Administrator (Learning Management System)",
+      "Learning & Development Analyst",
+      "Workforce Development Coordinator",
+      "L&D Project Coordinator",
+      "Development Administrator",
+      "Program Editor"
+    ],
+    "Specialist & Consulting Roles": [
+      "Learning and Development Business Partner",
+      "Learning and Development Consultant",
+      "Organizational Development Specialist",
+      "Performance Consultant",
+      "Capability Consultant",
+      "People Development Partner",
+      "Culture and Engagement Consultant"
+    ],
+    "Emerging Roles & Niche Specialisms": [
+      "Personal Learning Experience Designer",
+      "Learning AI Specialist",
+      "Hybrid L&D Facilitator",
+      "Talent Development Specialist",
+      "Digital Learning Transformation Lead",
+      "Knowledge Manager",
+      "Learning Product Owner",
+      "Learning Operations Lead",
+      "Skills Development Partner",
+      "Competency Framework Specialist",
+      "Digital Learning Experience Manager",
+      "Learning Innovation Lead"
+    ],
+    "Entry-Level Roles": [
+      "Training Administrator",
+      "L&D Intern",
+      "Junior Instructional Designer",
+      "Assistant Trainer",
+      "Training Support Officer",
+      "Learning Delivery Assistant",
+      "Junior L&D Consultant"
+    ]
+  }
+};
